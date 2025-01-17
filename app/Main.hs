f89d6c5f8dd5267c2c79785c273930a546cbf64a
unknown <jomjom27@gmail.com>
2025-01-17 01:34:21 +0000

    implement logger

--- app/Main.hs
+++ app/Main.hs
@@ -7,10 +7,12 @@ import Types (Request(..))
 import Control.Concurrent
 import Control.Monad (forM_)
 import Data.IORef (newIORef)
+import System.Log.FastLogger (newFileLoggerSet, defaultBufSize)
 
 
 main :: IO ()
 main = do 
+    logger <- newFileLoggerSet defaultBufSize "request.log"
     processedCounter <- newIORef 0
     let q1 = EmptyQueue :: RequestQueue Request
     forServer <- newMVar q1
@@ -18,7 +20,7 @@ main = do
     forM_ [0..9] $ \i -> 
         forkIO (initClient i forServer 10)
     forM_ [1..3] $ \i -> 
-        forkIO (initServer i forServer processedCounter end)
+        forkIO (initServer i forServer processedCounter end logger)
     endSignal <- takeMVar end
     print endSignal
 
--- src/Server.hs
+++ src/Server.hs
@@ -8,36 +8,50 @@ import Control.Concurrent (MVar, takeMVar, newMVar, newEmptyMVar, putMVar, threa
 import System.Random
 import Data.Time.Clock (UTCTime, getCurrentTime, diffUTCTime)
 import Data.IORef (IORef, readIORef, writeIORef)
+import System.Log.FastLogger 
 
-initServer :: Int -> MVar (RequestQueue Request) -> IORef Int -> MVar String -> IO ()
-initServer name forServer processedCounter end = do 
+initServer :: Int -> MVar (RequestQueue Request) -> IORef Int -> MVar String -> LoggerSet-> IO ()
+initServer name forServer processedCounter end logger = do 
     queueLog <- takeMVar forServer
-    print (show name ++ "called")
     let (request, queue) = dequeue queueLog
+
     placeHolderForDefault <- newEmptyMVar
+    
     currentTime <- getCurrentTime
     let (requestDetail, responseSignal, requestTime) = parseRequest request placeHolderForDefault currentTime
-    print requestDetail
-    print (show currentTime)
     let diff = diffUTCTime currentTime requestTime
+    
     putMVar forServer queue 
+    
     let response = Response {
         responseData = "The ping was received and it took the server " ++ show diff ++ " to respond.",
         responseTime = currentTime
     }
+    
+    logRequest logger currentTime requestDetail
+
     putMVar responseSignal response
 
     currentCount <- readIORef processedCounter
     let newCount = if requestDetail == "no request" then currentCount else currentCount + 1
     writeIORef processedCounter newCount
-    putStrLn (show newCount ++ show name)
+
     if newCount >= 100
         then do
             putMVar end "Server terminated"
         else return ()
+    
     threadDelay 500000
-    initServer name forServer processedCounter end
+    initServer name forServer processedCounter end logFile
 
 parseRequest :: Maybe Request -> MVar Response -> UTCTime -> (String, MVar Response, UTCTime)
 parseRequest (Just request) _ _ = (requestDetail request, responseSignal request, requestTime request)
 parseRequest Nothing defaultMVar defaultTime = ("no request", defaultMVar, defaultTime)
+
+logRequest :: LoggerSet -> UTCTime -> String -> IO ()
+logRequest logger date message = do
+    if message == "no request"
+        then return ()
+        else do
+            let logMessage = "[" ++ show date ++ "] : " ++ message ++ "\n"
+            pushLogStrLn logger (toLogStr logMessage)
